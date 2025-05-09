4f7de6109f6facf3e3e6dd4bce604e096badb337
Mohit Tiwari <mmjsmohit@gmail.com>
2025-05-09 15:01:12 +0530

    Add NumberFlow integration and enhance styling in App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NumberFlow, { continuous, NumberFlowGroup } from "@number-flow/react";
+import { useState } from "react";
 
 function App() {
+  const [productsSold, setProductsSold] = useState(0);
+  const [smootherSkin, setSmootherSkin] = useState(0);
+  const [glowUps, setGlowUps] = useState(0);
+  const [glowUpsEnd, setGlowUpsEnd] = useState(false);
+
+  const handleStartAnimation = () => {
+    setProductsSold(5);
+    setSmootherSkin(2);
+    setGlowUps(Number.MAX_SAFE_INTEGER);
+    setGlowUpsEnd(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NumberFlowGroup>
+        <div className="stats-container">
+          <div className="stat-item">
+            <div className="stat-value">
+              <NumberFlow
+                className="number-flow-value"
+                value={productsSold}
+                spinTiming={{ duration: 2000 }}
+              />
+              <span className="number-flow-suffix">M+</span>
+            </div>
+            <span className="stat-label">Products Sold</span>
+          </div>
+
+          <div className="stat-item">
+            <div className="stat-value">
+              <NumberFlow
+                className="number-flow-value"
+                value={smootherSkin}
+                spinTiming={{ duration: 2000 }}
+              />
+              <span className="number-flow-suffix">X</span>
+            </div>
+            <span className="stat-label">Smoother Skin</span>
+          </div>
+
+          <div className="stat-item">
+            <div className="stat-value">
+              <NumberFlow
+                format={{
+                  minimumIntegerDigits: 1,
+                  compactDisplay: "short",
+                  notation: "compact",
+                }}
+                className="number-flow-value"
+                value={glowUps}
+                spinTiming={{ duration: 2000 }}
+                onAnimationsFinish={() => setGlowUpsEnd(true)}
+              />
+            </div>
+            <span className="stat-label">Glow-Ups</span>
+          </div>
+        </div>
+      </NumberFlowGroup>
+      <button
+        onClick={handleStartAnimation}
+        style={{ marginTop: "20px", padding: "10px 20px", fontSize: "1rem" }}
+      >
+        Start Animations
+      </button>
     </div>
   );
 }
